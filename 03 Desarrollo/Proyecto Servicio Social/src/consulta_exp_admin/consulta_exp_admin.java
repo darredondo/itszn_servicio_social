49abc3dd6675a01a2b1e1e405038e858ab5a6894
cesararath <nigromante100000@gmail.com>
2023-06-07 16:51:29 -0600

    se redujo el numero de columnas

--- 03 Desarrollo/Proyecto Servicio Social/src/consulta_exp_admin/consulta_exp_admin.java
+++ 03 Desarrollo/Proyecto Servicio Social/src/consulta_exp_admin/consulta_exp_admin.java
@@ -40,13 +40,13 @@ public class consulta_exp_admin extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Title 1", "Title 2", "Title 3"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
